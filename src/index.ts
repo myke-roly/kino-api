19109d04c1ab9234c5712b26beb81e5f00bafe3a
myke-roly <myke_roly@hotmail.com>
2020-12-24 14:25:13 -0300

    use cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 dotenv.config();
 
 import { connectDB } from './db';
@@ -9,12 +10,13 @@ import { connectDB } from './db';
 import { authRouter, initialRoute } from './routes';
 
 connectDB();
-console.log('HELLO');
 const app: Express = express();
+console.log('kino api is starting....');
 const PORT = process.env.PORT || 8080;
 
 // middlewares
 app.use(morgan('common'));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
