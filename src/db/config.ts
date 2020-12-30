a8ddc1638d7f0f7d93746665e49207ba1ea51094
myke-roly <myke_roly@hotmail.com>
2020-12-30 06:39:57 -0300

    use atlas

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,8 +1,8 @@
 export default {
   jwtSecret: process.env.JWT_SECRET || 'SECRET_TOKEN',
   DB: {
-    URI_LOCAL: process.env.URI_LOCAL,
-    URI: process.env.MONGODB_URI || 'mongodb://localhost/kinodb',
+    // URI_LOCAL: process.env.URI_LOCAL,
+    URI: process.env.MONGODB_URI,
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD,
   },
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,15 @@ import { connect } from 'mongoose';
 import config from './config';
 
 export const connectDB = async (): Promise<any> => {
-  await connect(config.DB.URI_LOCAL, {
-    useFindAndModify: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  await connect(
+    config.DB.URI,
+    {
+      useFindAndModify: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => console.log(err)
+  )
     .then((db) => console.log(`Data base connected on port ${db.connection.host}`))
     .catch((error) => console.log(error));
 };
